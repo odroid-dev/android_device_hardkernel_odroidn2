e0f8c621fd5ce62b9f3ca9d3a08fa308b767aafd
secuflag <secuflag@ticino.com>
2020-10-16 14:24:24 +0200

    Update N2Plus checking

--- recovery/change_cpufreq.sh
+++ recovery/change_cpufreq.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
-HARDWARE=`cat /proc/cpuinfo | grep Hardware`
-N2PLUS="N2Plus"
+#!/sbin/sh
 
-if [ -z "${HARDWARE##*$N2PLUS*}" ]
-then
+HARDWARE=`cat /proc/cpuinfo | grep Hardware`
+
+if [[ $HARDWARE == *"N2Plus"* ]]; then
     sed -i s/_big=\"1800\"/_big=\"2208\"/g /sdcard/env.ini;
     sed -i s/_little=\"1896\"/_little=\"1908\"/g /sdcard/env.ini;
 fi
